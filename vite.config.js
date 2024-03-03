c11327d482576d3ceb3d8f1e657355e1d1b4fa0b
Mihail <mihail_korolev1111@mail.ru>
2024-03-03 23:23:26 +0300

    нчего

--- vite.config.js
+++ vite.config.js
@@ -3,34 +3,33 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 const vitePWA =VitePWA({
-  registerType: "autoUpdate",
-  
+  registerType:"autoUpdate",
     workbox:{
       globPatterns:[
         '**/*{css,woff2,png,svg,jpg,jpeg,js}'
     ],
     },
-
   manifest:{
     name: "cooking PWA for college project",
     short_name: "recipes",
-    description: "",
+    description: "recipes",
     theme_color: "ffffff",
 
-    icons: [
+    icons:[
       {
-        src: 'icons/android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png'
+      src:"icons/android-chrome-192x192.png",
+      sizes:"192x192",
+      type:"image/png"
       },
       {
-        src: 'icons/android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png'
-      }
-    ]
+      src:"icons/android-chrome-512x512.png",
+      sizes:"512x512",
+      type:"image/png" 
+      }]
+
   }
-})
+},
+  {})
 
 // https://vitejs.dev/config/
 export default defineConfig({
